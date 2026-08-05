f3b63e5640dff73c6ffd711938f7e72a0f5ade25
Guigui357 <zebuchinho77@gmail.com>
2026-08-05 14:59:42 -0300

    Update MoneroMiner.cpp

--- MoneroMiner/MoneroMiner.cpp
+++ MoneroMiner/MoneroMiner.cpp
@@ -1107,13 +1107,3 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 */
-
-// Deixe a main visível para o linker, mas sem código de loop para não iniciar sozinha!
-extern "C" {
-    int main(int argc, char* argv[]) {
-        (void)argc;
-        (void)argv;
-        Utils::threadSafePrint("[WASM] Subsistema de Threads do Emscripten pronto para comandos.", true);
-        return 0;
-    }
-}
